66d23f5151c6a2e1ccdcb9e3c9b7ee05d321e020
Kayla <6lackmacaroon@gmail.com>
2019-10-22 16:15:54 -0600

    axios post request to move/ endpoint

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './Button'
-
+import axios from 'Axios'
 
 const Controls = () => {
+  
+
+const baseUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/'
+
+handleDirection = direction => {
+  axios({
+    url: `${baseUrl}move/ `,
+    method: "POST",
+    headers: {
+      Authorization: `${key}`
+    },
+    data: {
+      direction: direction
+    }
+  })
+}
+
   return (
     <div>
       <p>controls here</p>
-      <Button name="North"/>
-      <Button name="South"/>
-      <Button name="East"/>
-      <Button name="West"/>
+      <Button name="North" onClick={() => this.handleDirection("n")}><i className="fas fa-arrow-up"/></Button>
+      <Button name="South" onClick={() => this.handleDirection("s")}><i className="fas fa-arrow-down"/></Button>
+      <Button name="East" onClick={() => this.handleDirection("e")}><i className="fas fa-arrow-right"/></Button>
+      <Button name="West" onClick={() => this.handleDirection("w")}><i className="fas fa-arrow-left"/></Button>
     </div>
   )
 }
