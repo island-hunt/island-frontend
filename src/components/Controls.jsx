d79474a462cb6c4ef7598d337f75a4c072f240c5
Andrew Benedict <atbenedict@gmail.com>
2019-10-22 15:34:07 -0400

    base layout

---
+++ src/components/Controls.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+
+
+const Controls = () => {
+  return (
+    <div>
+      <p>controls here</p>
+      <Button name="North"/>
+      <Button name="South"/>
+      <Button name="East"/>
+      <Button name="West"/>
+    </div>
+  )
+}
+
+export default Controls
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Controls from './Controls'
+import axios from 'axios'
+
+const key = process.env.SECRET_KEY
+const baseUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/'
+const headthing = `Token ${key}`;
+
+
+const Main = () => {
+  return (
+    <div>
+      <p>This is the main content</p>
+      <Controls/>
+    </div>
+  )
+}
+
+export default Main
