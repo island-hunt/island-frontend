339bb5b3bf4bb449d16e1a715aafd28e5ffa1fa8
Kayla <6lackmacaroon@gmail.com>
2019-10-23 12:57:44 -0600

    debugging cannot read property 'map' of undefined in maps.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,6 +7,16 @@ const Map = ({allRooms}) => {
         return (
           <div className="map-wrap">
             <p>It's a Map.</p>
+            <div
+              key={room.room_id}
+              className="rooms-div"
+              style={{
+                borderRight: room.exits.includes("e") ? "none" : "2px solid #e3e3e3",
+                borderLeft: room.exits.includes("w") ? "none" : "2px solid #e3e3e3",
+                borderTop: room.exits.includes("n") ? "none" : "2px solid #e3e3e3",
+                borderBottom: room.exits.includes("s") ? "none" : "2px solid #e3e3e3",
+              }}
+            />
             <p>{room.exits}
               {room.title === allRooms.title ? (
                 <i id='av-street-view' className="fa fa-street-view"></i>
