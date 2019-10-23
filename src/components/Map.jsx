ab57e52827e2f27d2408a932fcef8ba8e65a908c
Kayla <6lackmacaroon@gmail.com>
2019-10-23 11:35:12 -0600

    map rooms debug

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const Map = () => {
-    return (
-      <div className="map-wrap">
-        <p>It's a Map.</p>
-      </div>
-    )
+const Map = (props) => {
+  return(
+    <div className="mapper-wrapper">
+      {/* {props.allRooms.map(room => {
+        return ( */}
+          <div className="map-wrap">
+            <p>It's a Map.</p>
+            {/* <p>{room.exits}
+              {room.title === props.title ? (
+                <i id='av-street-view' className="fa fa-street-view"></i>
+              ) : (
+                  <i id='av-circle' className="fas fa-circle"></i>
+                )}
+            </p> */}
+          </div>
+      {/*}  )
+      })} */}
+    </div>
+  )
+      
 }
   
 export default Map
