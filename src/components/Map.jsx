568da2aaba015d2e771ddcda0595e9a7d1f3713f
Kayla <6lackmacaroon@gmail.com>
2019-10-23 15:35:39 -0600

    db dbug still not mapping

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Map = ({allRooms}) => {
   return(
     <div className="mapper-wrapper">
-      {allRooms.map(room => {
+      {/* {allRooms.map(room => {
         console.log(room)
          
-        return (
+        return ( */}
           <div className="map-wrap">
             <p>It's a Map.</p>  {/* SANITY CHECK */}
-            <div
+            {/* <div
               key={room.room_id}
               className="room-wrap"
               style={{
@@ -18,18 +18,18 @@ const Map = ({allRooms}) => {
                 borderTop: room.exits.includes("n") ? "none" : "2px solid #e3e3e3",
                 borderBottom: room.exits.includes("s") ? "none" : "2px solid #e3e3e3",
               }}
-            >
-              <p classname="room-icon">
+            > */}
+              {/* <p classname="room-icon">
                 {room.title === allRooms.title ? (
                   <i id='fa-street-view' className="fa fa-street-view"></i>
                 ) : (
                     <i id='fa-circle' className="fas fa-circle"></i>
                   )}
               </p> 
-            </div>
+            </div> */}
           </div>
-        )
-      })}
+      {/*    )
+       })} */}
     </div>
   )
       
