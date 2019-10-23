eb374ff66b81e8b8c8da39df1f55e4ec712fd837
Kayla <6lackmacaroon@gmail.com>
2019-10-23 14:37:59 -0600

    map scaffold built

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,26 +4,29 @@ const Map = ({allRooms}) => {
   return(
     <div className="mapper-wrapper">
       {allRooms.map(room => {
+        console.log(room)
+         
         return (
           <div className="map-wrap">
-            <p>It's a Map.</p>
+            <p>It's a Map.</p>  {/* SANITY CHECK */}
             <div
               key={room.room_id}
-              className="rooms-wrap"
+              className="room-wrap"
               style={{
                 borderRight: room.exits.includes("e") ? "none" : "2px solid #e3e3e3",
                 borderLeft: room.exits.includes("w") ? "none" : "2px solid #e3e3e3",
                 borderTop: room.exits.includes("n") ? "none" : "2px solid #e3e3e3",
                 borderBottom: room.exits.includes("s") ? "none" : "2px solid #e3e3e3",
               }}
-            />
-            <p>{room.exits}
-              {room.title === allRooms.title ? (
-                <i id='av-street-view' className="fa fa-street-view"></i>
-              ) : (
-                  <i id='av-circle' className="fas fa-circle"></i>
-                )}
-            </p>
+            >
+              <p classname="room-icon">
+                {room.title === allRooms.title ? (
+                  <i id='fa-street-view' className="fa fa-street-view"></i>
+                ) : (
+                    <i id='fa-circle' className="fas fa-circle"></i>
+                  )}
+              </p> 
+            </div>
           </div>
         )
       })}
